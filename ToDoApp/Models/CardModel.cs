66dd3d99b3c9e0c519ee169974a91ba4032470c8
Radoslav Kochev <radoslav.kochev@altiant.com>
2020-05-13 15:23:42 +0300

    initial commit

---
+++ ToDoApp/Models/CardModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ToDoApp.Models
+{
+    public class CardModel
+    {
+        public int Id { get; set; }
+        public CardStatuses Status { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public enum CardStatuses
+    {
+        Todo,
+        Started,
+        Completed
+    }
+}
